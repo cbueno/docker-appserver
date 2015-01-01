a6df3fc95bc9000297d75324b161d0d466162198
Cristóferson Bueno <cbueno81@gmail.com>
2015-01-01 19:32:02 -0200

    Removing jolokia

--- tomcat/6.0/deploy-and-run.sh
+++ tomcat/6.0/deploy-and-run.sh
@@ -10,5 +10,6 @@ if [ -d $DIR ]; then
 fi
 
 # Use faster (though more unsecure) random number generator
-export CATALINA_OPTS="$CATALINA_OPTS $(jolokia_opts) -Djava.security.egd=file:/dev/./urandom"
+# export CATALINA_OPTS="$CATALINA_OPTS $(jolokia_opts) -Djava.security.egd=file:/dev/./urandom"
+export CATALINA_OPTS="$CATALINA_OPTS -Djava.security.egd=file:/dev/./urandom"
 /opt/tomcat/bin/catalina.sh run
